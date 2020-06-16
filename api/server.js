6216a00e0ec1c62c756fdfd28f139923da189f47
Michael Perez <mperez6473@gmail.com>
2020-06-16 15:29:40 -0500

    Have the files setup with the database running. The token bit needs to be added still.

---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const reqAuth = require("../auth/reqAuth");
+const KnexSessionsStore = require("connect-session-knex")(session);
+const usersRouter = require("../users/usersRouter.js");
+const authRouter = require("../auth/authRouter");
+const dbConnection = require("../data/connection");
+const server = express();
+const sessionConfig = {
+    name: "monster",
+    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+    cookie: {
+        maxAge: 1000 * 600,
+        secure: process.env.COOKIE_SECURE || false, //  true means only use over https //  true in production
+        httpOnly: true, //JS code on the client cannot access the session cookie
+    }, // 10 min in milliseconds
+    resave: false,
+    saveUninitialiezed: false, //  GDPR compliance
+    store: new KnexSessionsStore({
+        knex: dbConnection,
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 6000, //  delete expired sessions - in milliseconds
+    }),
+};
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/users", reqAuth, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "home slash of the login page" });
+});
+module.exports = server;
---
+++ auth/authRouter.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const bcryptjs = require("bcrypt");
+
+const Users = require("../users/usersModel");
+
+router.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    //  hash user password
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(password, rounds);
+    Users.add({ username, password: hash })
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => res.send(err));
+});
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    //  verify user password
+    Users.findBy({ username })
+        .then(([user]) => {
+            console.log(user);
+            req.session.user = { user };
+            if (user && bcryptjs.compareSync(password, user.password)) {
+                res.status(200).json({ user, session: req.session });
+            } else
+                res.status(401).json({
+                    Error:
+                        "The username and password combination was not found in our database.",
+                });
+        })
+        .catch((err) => {
+            // console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({
+                    Error: "Could not log out, please try again...",
+                });
+            } else res.status(204).end();
+        });
+    } else {
+        req.status(204).end();
+    }
+});
+module.exports = router;
