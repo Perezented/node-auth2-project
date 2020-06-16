74280f131de9d18574876670f8b665f69f0df31f
Michael Perez <mperez6473@gmail.com>
2020-06-16 17:49:20 -0500

    Have the users router require the token in the auth.

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 const Users = require("./usersModel.js");
-
-router.get("/", (req, res) => {
+const restrictedLoggedIn = require("../auth/restrictedMiddleware");
+router.get("/", restrictedLoggedIn, (req, res) => {
     Users.find()
         .then((users) => {
             res.status(200).json(users);
