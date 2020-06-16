5ff46c91d7ad2627d67056b069604acc818baede
Michael Perez <mperez6473@gmail.com>
2020-06-16 16:42:32 -0500

    Patched up the tables and have it lookin a lil better and simplier.

--- data/migrations/20200615141243_init.js
+++ data/migrations/20200615141243_init.js
@@ -1,26 +1,16 @@
 exports.up = function (knex) {
     return knex.schema
-        .createTable("department", (tbl) => {
-            tbl.increments();
-
-            tbl.string("name", 128).notNullable().unique();
-        })
         .createTable("users", (tbl) => {
             tbl.increments();
 
             tbl.string("username", 128).notNullable().unique().index();
             tbl.string("password", 256).notNullable();
 
-            tbl.integer("department")
-                .unsigned()
-                .references("department.id")
-                .onDelete("RESTRICT")
-                .onUpdate("CASCADE");
-        });
+            tbl.string("department", 256);
+        })
+        .dropTableIfExists("description");
 };
 
 exports.down = function (knex) {
-    return knex.schema
-        .dropTableIfExists("department")
-        .dropTableIfExists("users");
+    return knex.schema.dropTableIfExists("users");
 };
